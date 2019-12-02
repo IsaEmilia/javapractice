70633bec00ce7f725a5130e82ed624784b4dac3f
isa <isaemilia.h@gmail.com>
2019-12-02 14:47:06 +0200

    Beautified.

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const attempt = document.getElementById("fails");
 const succes = document.getElementById("answer");
 const button = document.getElementById("button");
 
-let randomNum = Math.floor(Math.random() *101);
+let randomNum = Math.floor(Math.random() * 101);
 console.log(randomNum);
 
 
- function guessNum(event){
-     event.preventDefault()
-    
-    const guess = input.value 
+function guessNum(event) {
+    event.preventDefault()
 
-   
-    if(guess == randomNum) {
+    const guess = input.value
+
+
+    if (guess == randomNum) {
         succes.innerHTML += ` ${guess} `;
         button.innerHTML = "VICTORY!"
         button.disabled = true;
-    }else if(guess >= randomNum) {
+    } else if (guess >= randomNum) {
         attempt.innerHTML += ` ${guess} `;
         button.innerHTML = "lower!"
-    }else if( guess <= randomNum) {
+    } else if (guess <= randomNum) {
         attempt.innerHTML += ` ${guess} `;
         button.innerHTML = "higher!"
     }
