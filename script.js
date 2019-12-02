ff9af6a9e143dedca84ec26e5f717e59bdaf941c
isa <isaemilia.h@gmail.com>
2019-12-02 13:47:44 +0200

    Add some variables

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+const guessInput = document.querySelector(".guessInput");
+const input = document.getElementById("name");
+const attempt = document.querySelector(".attempts");
+const succes = document.querySelector(".succes");
 
-console.log("hello")
+let randomNum = Math.floor(Math.random() *101);
+console.log(randomNum);
 
-let randomNum = Math.floor(Math.random() *101)
 
-console.log(randomNum)
- function guessNum(){
-     
- }
+ function guessNum(event){
+     event.preventDefault()
+console.log(input.value)
+    console.log("hello")
+}
+
+
+function correct(){
+
+}
+
+
+
+
+
+guessInput.addEventListener("submit", guessNum);
